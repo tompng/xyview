15646ad469ea3f5a0fc7e7067a3399c96486bb76
tompng <tomoyapenguin@gmail.com>
2022-05-08 01:17:41 +0900

    fix example, npm udpate

--- example/example.ts
+++ example/example.ts
@@ -15,15 +15,15 @@ onload = () => {
     const sizePerPixel = view.viewport.sizePerPixel
     if (e.ctrlKey) {
       const ratio = 1.01 ** e.deltaY
-      view.update({ viewport: { sizePerPixel: { width: sizePerPixel.width * ratio, height: sizePerPixel.height * ratio }}, calcPaused: true })
+      view.update({ viewport: { sizePerPixel: { x: sizePerPixel.x * ratio, y: sizePerPixel.y * ratio }}, calcPaused: true })
       if (timer) clearTimeout(timer)
       timer = window.setTimeout(() => view.update({ calcPaused: false }), 100)
     } else {
       view.update({
         viewport: {
           center: {
-            x: view.viewport.center.x + e.deltaX * sizePerPixel.width,
-            y: view.viewport.center.y - e.deltaY * sizePerPixel.height
+            x: view.viewport.center.x + e.deltaX * sizePerPixel.x,
+            y: view.viewport.center.y - e.deltaY * sizePerPixel.y
           }
         }
       })
