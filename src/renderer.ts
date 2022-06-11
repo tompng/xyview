aca6785066d6dabbb6a689fb14879f651a046433
tompng <tomoyapenguin@gmail.com>
2022-06-11 19:52:15 +0900

    render 1D

--- src/renderer.ts
+++ src/renderer.ts
@@ -80,27 +80,29 @@ export function parseFormulas(expressions: string[]): ParsedFormula[] {
         const rDeps = extractVariables(right)
         if (lDeps.length <= 1 && rDeps.length <= 1 && lDeps[0] !== rDeps[0]) {
           if (typeof left === 'string') {
-            const valueFuncCode = astToValueFunctionCode(ast, [left])
+            const axis = left === 'x' ? 'y' : 'x'
+            const valueFuncCode = astToValueFunctionCode(right, [axis])
             const valueFunc: ValueFunction1D = eval(valueFuncCode)
-            const rangeFunc: RangeFunction1D = eval(astToRangeFunctionCode(ast, [left], rangeOption))
+            const rangeFunc: RangeFunction1D = eval(astToRangeFunctionCode(right, [axis], rangeOption))
             return {
               type: 'eq',
               key: `${left} left ${mode} ${valueFuncCode}`,
               valueFunc,
               rangeFunc,
-              calcType: `f${left as 'x' | 'y'}`,
+              calcType: `f${axis}`,
               fillMode
             }
           } else if (typeof right === 'string') {
-            const valueFuncCode = astToValueFunctionCode(ast, [right])
+            const axis = right === 'x' ? 'y' : 'x'
+            const valueFuncCode = astToValueFunctionCode(left, [axis])
             const valueFunc: ValueFunction1D = eval(valueFuncCode)
-            const rangeFunc: RangeFunction1D = eval(astToRangeFunctionCode(ast, [right], { pos: negative, neg: positive, eq: zero, zero }))
+            const rangeFunc: RangeFunction1D = eval(astToRangeFunctionCode(left, [axis], { pos: negative, neg: positive, eq: zero, zero }))
             return {
               type: 'eq',
               key: `${right} right ${mode} ${valueFuncCode}`,
               valueFunc,
               rangeFunc,
-              calcType: `f${right as 'x' | 'y'}`,
+              calcType: `f${axis}`,
               fillMode: { positive: negative, negative: positive, zero }
             }
           }
@@ -122,6 +124,14 @@ export function parseFormulas(expressions: string[]): ParsedFormula[] {
       return { type: 'error', error: String(e) }
     }
   })
+}
+
+function fillModeToMask(fillMode: FillMode) {
+  return (
+    (fillMode.positive ? (1 << RangeResults.POSITIVE) : 0) +
+    (fillMode.negative ? (1 << RangeResults.NEGATIVE) : 0) +
+    (fillMode.zero ? (1 << RangeResults.EQZERO) : 0)
+  )
 }
 
 type RenderingRange = { xMin: number; xMax: number; yMin: number; yMax: number }
@@ -156,8 +166,9 @@ export function render(
     ctx.globalAlpha = fillAlpha
   }
   const plotPoints: number[] = []
+  const skipCalcDot = !fillMode.negative && !fillMode.positive && !fillMode.zero
   function calcDot(xMin: number, xMax: number, yMin: number, yMax: number) {
-    if (!fillMode.negative && !fillMode.positive && !fillMode.zero) return
+    if (skipCalcDot) return
     const x0 = 0.75 * xMin + 0.25 * xMax
     const x1 = 0.25 * xMin + 0.75 * xMax
     const y0 = 0.75 * yMin + 0.25 * yMax
@@ -180,8 +191,8 @@ export function render(
   function calcFull(xMin: number, xMax: number, yMin: number, yMax: number, size: number) {
     const dx = (xMax - xMin) / size
     const dy = (yMax - yMin) / size
-    let values = new Array<number>(size + 1)
-    let nextValues = new Array<number>(size + 1)
+    let values: number[] = []
+    let nextValues: number[] = []
     for (let ix = 0; ix <= size; ix++) values[ix] = valueFunc(xMin + ix * dx, yMin)
     for (let iy = 1; iy <= size; iy++) {
       const y1 = yMin + iy * dy
@@ -219,11 +230,7 @@ export function render(
       ;[values, nextValues] = [nextValues, values]
     }
   }
-  const fillMask = (
-    (fillMode.positive ? (1 << RangeResults.POSITIVE) : 0) +
-    (fillMode.negative ? (1 << RangeResults.NEGATIVE) : 0) +
-    (fillMode.zero ? (1 << RangeResults.EQZERO) : 0)
-  )
+  const fillMask = fillModeToMask(fillMode)
   let ranges = [range.xMin, range.xMax, range.yMin, range.yMax]
   let currentSize = size
   while (currentSize >= 1) {
@@ -265,6 +272,124 @@ export function render(
   ctx.fill()
 }
 
+function calc1DRange(formula: ParsedEquation1D, size: number, min: number, max: number) {
+  const { valueFunc, rangeFunc, fillMode } = formula
+  const fillMask = fillModeToMask(fillMode)
+  type Range = [number, number]
+  let ranges: Range[] = [[min, max]]
+  let currentSize = size
+  const fills: Range[] = []
+  const alphaFills: [number, number, number][] = []
+  const plots: number[] = []
+  const { BOTH } = RangeResults
+  const skipCalcDot = !fillMode.negative && !fillMode.positive && !fillMode.zero
+  function calcDot(min: number, max: number) {
+    if (skipCalcDot) return
+    const v0 = valueFunc((min * 7 + max) / 8)
+    const v1 = valueFunc((min * 5 + 3 * max) / 8)
+    const v2 = valueFunc((min * 3 + 5 * max) / 8)
+    const v3 = valueFunc((min + 8 * max) / 8)
+    let alpha = 0
+    if (fillMode.zero) {
+      alpha += ((v0 === 0 ? 1 : 0) + (v1 === 0 ? 1 : 0) + (v2 === 0 ? 1 : 0) + (v3 === 0 ? 1 : 0)) / 4
+    }
+    if (fillMode.negative) {
+      alpha += ((v0 < 0 ? 1 : 0) + (v1 < 0 ? 1 : 0) + (v2 < 0 ? 1 : 0) + (v3 < 0 ? 1 : 0)) / 4
+    } else if (fillMode.positive) {
+      alpha += ((v0 > 0 ? 1 : 0) + (v1 > 0 ? 1 : 0) + (v2 > 0 ? 1 : 0) + (v3 > 0 ? 1 : 0)) / 4
+    }
+    if (alpha > 0) alphaFills.push([min, max, alpha])
+  }
+  function calcFull(min: number, max: number, size: number) {
+    const d = (max - min) / size
+    let values: number[] = []
+    for (let i = 0; i <= size; i++) values[i] = valueFunc(min + i * d)
+    for (let i = 0; i < size; i++) {
+      const x0 = min + i * d
+      const x1 = x0 + d
+      const v0 = values[i]
+      const v1 = values[i + 1]
+      if (fillMode.negative || fillMode.positive || fillMode.zero) {
+        if (fillMode.zero && valueFunc(x0 + d / 2) === 0) {
+          fills.push([x0, x1])
+        } else if (fillMode.negative) {
+          if (v0 < 0 && v1 < 0) {
+            fills.push([x0, x1])
+          } else if (v0 < 0 || v1 < 0 || v1 < 0 || v1 < 0) {
+            calcDot(x0, x1)
+          }
+        } else if (fillMode.positive) {
+          if (v0 > 0 && v1 > 0) {
+            fills.push([x0, x1])
+          } else if (v0 > 0 || v1 > 0) {
+            calcDot(x0, x1)
+          }
+        }
+      }
+      if (v0 * v1 <= 0 && v0 !== v1) plots.push(x0 - d * v0 / (v1 - v0))
+    }
+  }
+  while (currentSize >= 1 && ranges.length) {
+    const nextRanges: Range[] = []
+    for (const [min, max] of ranges) {
+      const result = rangeFunc(min, max)
+      if (result >= 0) {
+        if (((fillMask >> result) & 1) === 1) fills.push([min, max])
+      } else if (result === BOTH && currentSize <= 64) {
+        calcFull(min, max, currentSize)
+      } else if (currentSize > 1) {
+        const center = (min + max) / 2
+        nextRanges.push([min, center], [center, max])
+      } else {
+        calcDot(min, max)
+      }
+    }
+    ranges = nextRanges
+    currentSize /= 2
+  }
+  return { fills, plots, alphaFills }
+}
+
+function calc1DValues(formula: ParsedEquation1D, size: number, min: number, max: number) {
+  const { valueFunc, rangeFunc } = formula
+  type Range = [number, number]
+  let ranges: Range[] = [[min, max]]
+  let currentSize = size
+  const curves: number[][] = []
+  const { BOTH, EQNAN } = RangeResults
+  function calcFull(min: number, max: number, size: number){
+    const d = (max - min) / size
+    let curve: number[] = []
+    curves.push(curve)
+    for (let i = 0; i <= size; i++) {
+      const x = min + d * i
+      const v = valueFunc(x)
+      if (isNaN(v)) {
+        curve = []
+        curves.push(curve)
+      } else {
+        curve.push(x, v)
+      }
+    }
+  }
+  while (currentSize >= 1 && ranges.length) {
+    const nextRanges: Range[] = []
+    for (const [min, max] of ranges) {
+      const result = rangeFunc(min, max)
+      if (result === EQNAN) continue
+      if (result >= 0 || result === BOTH) {
+        calcFull(min, max, currentSize)
+      } else if (currentSize > 1) {
+        const center = (min + max) / 2
+        nextRanges.push([min, center], [center, max])
+      }
+    }
+    ranges = nextRanges
+    currentSize /= 2
+  }
+  return curves
+}
+
 export function render1D(
   canvas: HTMLCanvasElement,
   size: number,
@@ -277,6 +402,79 @@ export function render1D(
     fillAlpha: number
   }
 ) {
+  const xFactor = size / (range.xMax - range.xMin)
+  const xOffset = offset - size * range.xMin / (range.xMax - range.xMin)
+  const yFactor = size / (range.yMax - range.yMin)
+  const yOffset = offset - size * range.yMin / (range.yMax - range.yMin)
+  const { fillMode } = formula
+  const ctx = canvas.getContext('2d')!
+  ctx.save()
+  ctx.fillStyle = renderMode.color
+  const { fillAlpha, lineWidth } = renderMode
+  ctx.globalAlpha = fillAlpha
+  const isXCalc = formula.calcType === 'x' || formula.calcType === 'fx'
+  const [baseOffset, baseFactor, fOffset, fFactor] = isXCalc ? [xOffset, xFactor, yOffset, yFactor] : [yOffset, yFactor, xOffset, xFactor]
+  const [baseAxisMin, baseAxisMax] = isXCalc ? [range.xMin, range.xMax] : [range.yMin, range.yMax]
+  if (!isXCalc) {
+    ctx.rotate(Math.PI / 2)
+    ctx.scale(1, -1)
+  }
+  ctx.beginPath()
+  ctx.rect(0, offset, 2 * offset + size, size)
+  ctx.clip()
+  if (formula.calcType === 'x' || formula.calcType === 'y') {
+    const { fills, plots, alphaFills } = calc1DRange(formula, size, baseAxisMin, baseAxisMax)
+    const fill = (a: number, b: number) => ctx.fillRect(baseOffset + baseFactor * a, offset, baseFactor * (b - a), size)
+    const plot = (a: number) => ctx.fillRect(baseOffset + baseFactor * a - lineWidth / 2, offset, lineWidth, size)
+    ctx.fillStyle = ctx.strokeStyle = renderMode.color
+    ctx.globalAlpha = fillAlpha
+    for (const [min, max] of fills) fill(min, max)
+    for (const [min, max, alpha] of alphaFills) {
+      ctx.globalAlpha = alpha * fillAlpha
+      fill(min, max)
+    }
+    ctx.globalAlpha = 1
+    for (const v of plots) plot(v)
+  } else {
+    const curves = calc1DValues(formula, size, baseAxisMin, baseAxisMax)
+    ctx.fillStyle = ctx.strokeStyle = renderMode.color
+    ctx.globalAlpha = fillAlpha
+    if (fillMode.positive || fillMode.negative) {
+      for (const curve of curves) {
+        if (curve.length <= 2) continue
+        ctx.beginPath()
+        const f = fillMode.negative ? 0 : 2 * offset + size
+        const base0 = curve[0]
+        const base1 = curve[curve.length - 2]
+        ctx.moveTo(baseOffset + baseFactor * base1, f)
+        ctx.lineTo(baseOffset + baseFactor * base0, f)
+        for (let i = 0; i < curve.length; i+= 2) {
+          ctx.lineTo(baseOffset + baseFactor * curve[i], fOffset + fFactor * curve[i + 1])
+        }
+        ctx.fill()
+      }
+    }
+    ctx.globalAlpha = 1
+    ctx.lineWidth = lineWidth
+    ctx.lineJoin = ctx.lineCap = 'round'
+    for (const curve of curves) {
+      if (curve.length === 0) continue
+      const bs = baseOffset + baseFactor * curve[0]
+      const fs = fOffset + fFactor * curve[1]
+      if (curve.length === 2) {
+        ctx.beginPath()
+        ctx.arc(bs, fs, lineWidth / 2, 0, 2 * Math.PI)
+        ctx.fill()
+      } else {
+        ctx.beginPath()
+        ctx.moveTo(bs, fs)
+        for (let i = 2; i < curve.length; i+=2) {
+          ctx.lineTo(baseOffset + baseFactor * curve[i], fOffset + fFactor * curve[i + 1])
+        }
+        ctx.stroke()
+      }
+    }
 
-
+  }
+  ctx.restore()
 }
